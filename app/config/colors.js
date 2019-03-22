d9103643df25fb3c3ea42aca280dd992dfbe4ca1
Sergejs0728 <sergejs.nescereckis0728@gmail.com>
2019-03-22 10:44:27 +1000

    commit

---
+++ app/config/colors.js
@@ -0,0 +1,21 @@
+export default {
+  red: '#ED675A',
+  green: '#03DBBB',
+  black: '#4D4D4D',
+  darkblue: '#2470A0',
+  lightblue: '#3D95CE',
+  lightgray: '#EBEBEB',
+  darkgray: '#4D4D4D',
+  lightergray: '#F6F6F6',
+  darkestgray: '#A6A6A6',
+  darkergray: '#D8D8D8',
+  drawerHeaderText: '#C0C9CF',
+  transactionBackground: '#F7F7F7',
+  whitesmoke: '#F6F6F6',
+  drawerColor: '#333B42',
+  gold: '#F8CF51',
+
+  primary: '#3D95CE',
+  secondary: '#F8CF51',
+  tertiary: '#2470A0',
+};
